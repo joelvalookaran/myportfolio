83bb19e9d2cb500a1806679be83d8416131c0480
joel vj <joelvj34@gmail.com>
2025-08-24 16:03:49 +0530

    Update Drawer.js
    Hii

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -106,7 +106,7 @@ export default function Drawer({ onPageChange, currentPage }) {
             />
           </Box>
           <Typography variant="h6" sx={{ fontWeight: 800, color: '#e6f0ff', textAlign: 'center' }}>
-            Rithin Reji ENGg
+            Joel 
           </Typography>
 
           <Stack direction="row" spacing={1.5} sx={{ pt: 1 }}>
@@ -140,7 +140,7 @@ export default function Drawer({ onPageChange, currentPage }) {
           <Typography variant="caption" sx={{ color: '#9fb1c1' }}>
             Designed by{' '}
             <MuiLink href="#" underline="hover" sx={{ color: '#7fb1ff', fontWeight: 600 }}>
-              Kishan Nayak
+              Joel 
             </MuiLink>
           </Typography>
         </Box>
